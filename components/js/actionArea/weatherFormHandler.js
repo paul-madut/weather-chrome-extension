2ce4484429c49b9f4ba0bdc0d460c49d92bb6815
paul-madut <paul.madut@mths.ca>
2023-02-20 23:50:10 -0500

    not working yet

--- components/js/actionArea/weatherFormHandler.js
+++ components/js/actionArea/weatherFormHandler.js
@@ -8,6 +8,16 @@ let allData;
 let weatherForm = document.getElementById("form");
 let weatherFormButton = document.getElementById("weather-form-button");
 let form = document.getElementById("form-area");
+var gps ='';
+navigator.geolocation.getCurrentPosition(position => {
+  let loc = `${position.coords.longitude},${position.coords.latitude}`;
+  
+  let location = document.getElementById("location");
+  location = location.value;
+
+  location = loc
+  });
+console.log(gps)
 
 weatherForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -25,10 +35,15 @@ weatherForm.addEventListener("submit", (e) => {
   });
 });
 
+
 weatherFormButton.addEventListener("click", (e) => {
   e.preventDefault();
   let location = document.getElementById("location");
   location = location.value;
+  if(location == ''){
+    location = "1600 Pennsylvania Avenue NW, Washington, DC 20500, USA"
+  }
+  
   let loading = document.getElementById("loader");
   loading.style.display = "flex";
   form.style.display = "none";
